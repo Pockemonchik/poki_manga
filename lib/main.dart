c45b967a7515f89099e09a44dd0ac757d6b15643
Pockemonchik <19pokemonchik99@gmail.com>
2023-03-27 19:47:51 +0300

    firs_commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color(255),
       ),
-      home: LibraryScreen(),
+      home: LibraryScreen(), 
     );
   }
 }
