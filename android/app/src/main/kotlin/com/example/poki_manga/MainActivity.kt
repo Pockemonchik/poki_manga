c914cc230a65ef60235e189df1c0c4d24cc58406
Pockemonchik <19pokemonchik99@gmail.com>
2023-03-27 19:45:48 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/poki_manga/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poki_manga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Poki Manga!",
+      theme: ThemeData(
+        primaryColor: Color(255),
+      ),
+      home: LibraryScreen(),
+    );
+  }
+}
+
+class LibraryScreen extends StatelessWidget {
+  const LibraryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+    );
+  }
+}
